4a4ba104fe416a261d8f433644dfea96aac8a429
Coffeeteer <tgrieshiemer@gmail.com>
2017-01-21 15:19:08 -0500

    at 13:04 on the project

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left')
+
 function timer(seconds){
 	const now = Date.now();
 	const then = now + seconds * 1000;
+	dispalyTimeLeft(seconds);
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		// check to see if we should stop t
-		if(secondsLeft <= 0){
+		if(secondsLeft < 0){
 			clearInterval(countdown);
 			return;
 		}
 		// display
-		console.log(secondsLeft);
+		dispalyTimeLeft(secondsLeft);
 	}, 1000);
+}
+
+function dispalyTimeLeft(seconds){
+	const minutes = Math.floor(seconds / 60);
+	const remainderSeconds = seconds % 60;
+	const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+	document.title = display;
+	timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp){
+	const end = new Data(timestamp);
 }
