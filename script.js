aac91aa27d3e9d76692e92ba3cbb25fb3da7a5a9
Coffeeteer <tgrieshiemer@gmail.com>
2017-01-21 16:33:19 -0500

    finished project

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left')
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds){
+	//clear any existing timers
+	clearInterval(countdown)
 	const now = Date.now();
 	const then = now + seconds * 1000;
 	dispalyTimeLeft(seconds);
+	displayEndTime(then);
+
 	countdown = setInterval(() => {
 		const secondsLeft = Math.round((then - Date.now()) / 1000);
 		// check to see if we should stop t
@@ -26,5 +32,23 @@ function dispalyTimeLeft(seconds){
 }
 
 function displayEndTime(timestamp){
-	const end = new Data(timestamp);
+	const end = new Date(timestamp);
+	const hour = end.getHours();
+	const adjustedHour = hour > 12 ? hour - 12 : hour;
+	const minutes = end.getMinutes();
+	endTime.textContent = `Be Back at the ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
+
+function startTimer(){
+	const seconds = parseInt(this.dataset.time);
+	timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e){
+	e.preventDefault();
+	const mins = this.minutes.value;
+	console.log(mins);
+	timer(mins * 60);
+	this.reset();
+});
